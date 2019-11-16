f5e78b84f9a4ca43a7cef9fe8e87b6c38f6963be
Bjornskjald <github@bjorn.ml>
2019-11-16 15:15:08 +0200

    fixed algorithm for detecting keywords

--- android/HJ2019/app/src/main/java/io/github/feelfreelinux/hj2019/SuspiciousEventsDetector.kt
+++ android/HJ2019/app/src/main/java/io/github/feelfreelinux/hj2019/SuspiciousEventsDetector.kt
@@ -4,52 +4,68 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.content.ContextCompat
 
-data class Scenario(val keywords: List<String>, val hint: String)
+data class Scenario(val keywordsList: List<List<String>>, val hint: String)
 
 
 val SCENARIOS = listOf(
-    Scenario(listOf(
-        "school",
-        "address",
-        "home",
-        "live"
-    ), "Don't show reveal personal information to strangers!")
+    Scenario(
+        listOf(
+            listOf("school"),
+            listOf("address"),
+            listOf("home"),
+            listOf("live")
+        ), "Don't show reveal personal information to strangers!"
+    )
 )
 
 val SCENARIOS_OUTPUT_ANALYZED = listOf(
-    Scenario(listOf(
-        "school",
-        "address",
-        "home",
-        "live"
-    ), "Don't show reveal personal information to strangers!"),
-    Scenario(listOf(
-        "how old are you",
-        "how many years do you have",
-        "when have you borned"
-    ), "Don't give your age to strangers!"),
-    Scenario(listOf(
-        "Send",
-        "nudes"
-    ), "This doesn’t seems good, maybe leave this chat"),
-    Scenario(listOf(
-        "Are",
-        "your",
-        "parents",
-        "home"
-    ), "This is not a question that you are supposed to respond to"),
-    Scenario(listOf(
-        "Download this for free",
-        "candies",
-        "diamonds"
-    ), "Remember! Do not download anything from weird sites!"),
-    Scenario(listOf(
-        "Confirm that you are a human via sms"
-    ), "This is a scam, if you’ll give your phone number, you can lost your money!"),
-    Scenario(listOf(
-        "Union",
-        "transfer"
-    ), "Don't transfer money to strangers! If you want to buy something online - ask your parent")
+    Scenario(
+        listOf(
+            listOf("school"),
+            listOf("address"),
+            listOf("home"),
+            listOf("live")
+        ), "Don't show reveal personal information to strangers!"
+    ),
+    Scenario(
+        listOf(
+            listOf("how old are you"),
+            listOf("how many years do you have"),
+            listOf("when have you borned")
+        ), "Don't give your age to strangers!"
+    ),
+    Scenario(
+        listOf(
+            listOf("Send"),
+            listOf("nudes")
+        ), "This doesn’t seems good, maybe leave this chat"
+    ),
+    Scenario(
+        listOf(
+            listOf("Are"),
+            listOf("your"),
+            listOf("parents"),
+            listOf("home")
+        ), "This is not a question that you are supposed to respond to"
+    ),
+    Scenario(
+        listOf(
+            listOf("Download this for free"),
+            listOf("candies"),
+            listOf("diamonds")
+        ), "Remember! Do not download anything from weird sites!"
+    ),
+    Scenario(
+        listOf(
+            listOf("Confirm that you are a human via sms")
+        ), "This is a scam, if you’ll give your phone number, you can lost your money!"
+    ),
+    Scenario(
+        listOf(
+            listOf("Union"),
+            listOf("transfer")
+        ), "Don't transfer money to strangers! If you want to buy something online - ask your parent"
+    )
 )
 
 
@@ -57,43 +73,41 @@ class SuspiciousEventsDetector {
     companion object {
 
         fun eventTyped(data: String, context: Context) {
-            data.split(" ").forEach mainForEach@{
-                    splittedWord ->
-                SCENARIOS.forEach {
-                    scenario ->
-                    scenario.keywords.forEach {
-                        keyword ->
-                        if (JaroDistance.calculate(keyword.toLowerCase(), splittedWord.toLowerCase()) > 0.9) {
-                            val intent = Intent(context, FloatingBalloon::class.java)
+            SCENARIOS.forEach { scenario ->
+                if (checkKeywords(scenario, data)) {
+                    val intent = Intent(context, FloatingBalloon::class.java)
 
-                            intent.putExtra("HINT", scenario.hint)
-                            ContextCompat.startForegroundService(context, intent)
-                            return@mainForEach
-                        }
-                    }
+                    intent.putExtra("HINT", scenario.hint)
+                    ContextCompat.startForegroundService(context, intent)
+                    return
                 }
             }
         }
 
         fun textViewFound(id: String, data: String, context: Context) {
-            if (id == "com.instagram.android:id/direct_text_message_text_view" || id ==  "com.zhiliaoapp.musically:id/eyx") {
-                data.split(" ").forEach mainForEach@{
-                        splittedWord ->
-                    SCENARIOS_OUTPUT_ANALYZED.forEach {
-                            scenario ->
-                        scenario.keywords.forEach {
-                                keyword ->
-                            if (JaroDistance.calculate(keyword.toLowerCase(), splittedWord.toLowerCase()) > 0.9) {
-                                val intent = Intent(context, FloatingBalloon::class.java)
+            if (id == "com.instagram.android:id/direct_text_message_text_view" || id == "com.zhiliaoapp.musically:id/eyx") {
+                SCENARIOS_OUTPUT_ANALYZED.forEach { scenario ->
+                    if (checkKeywords(scenario, data)) {
+                        val intent = Intent(context, FloatingBalloon::class.java)
 
-                                intent.putExtra("HINT", scenario.hint)
-                                ContextCompat.startForegroundService(context, intent)
-                                return@mainForEach
-                            }
-                        }
+                        intent.putExtra("HINT", scenario.hint)
+                        ContextCompat.startForegroundService(context, intent)
+                        return
                     }
                 }
             }
+        }
+
+        private fun checkKeywords(scenario: Scenario, data: String): Boolean {
+            val splittedWords = data.split(" ").map { word -> word.toLowerCase() }
+            scenario.keywordsList.forEach { keywords ->
+                splittedWords.forEach { word ->
+                    if (keywords.map { kw -> JaroDistance.calculate(kw.toLowerCase(), word) }.all { d -> d > 0.9 }) {
+                        return true
+                    }
+                }
+            }
+            return false
         }
     }
 }
